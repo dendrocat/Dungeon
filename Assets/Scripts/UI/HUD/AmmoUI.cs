2e1ecdbc62f6ce9e4ef1dbb64a466353c5ed0ab7
dendrocat <dan.re0@mail.ru>
2026-05-14 13:55:17 +0300

    Added icons for hud and changed size for soldier and archer

--- Assets/Scripts/UI/HUD/AmmoUI.cs
+++ Assets/Scripts/UI/HUD/AmmoUI.cs
@@ -7,6 +7,7 @@ public class AmmoUI : HUDComponent
 
     [SerializeField] TextMeshProUGUI m_Ammo;
     [SerializeField] TextMeshProUGUI m_Grenade;
+    [SerializeField] UnityEngine.UI.Image m_GrenadeImage;
 
     public override void Init(Player player)
     {
@@ -22,5 +23,7 @@ public class AmmoUI : HUDComponent
     {
         UpdateAmmo(m_Ammo, m_WeaponHandler.Weapon);
         UpdateAmmo(m_Grenade, m_WeaponHandler.Grenade);
+        if (m_WeaponHandler.Grenade.IsReloading)
+            m_GrenadeImage.fillAmount = m_WeaponHandler.Grenade.ReloadProgress;
     }
 }
--- Assets/Scripts/UI/HUD/HealthUI.cs
+++ Assets/Scripts/UI/HUD/HealthUI.cs
@@ -6,19 +6,18 @@ public class HealthUI : HUDComponent
     PlayerHealth m_Health;
 
     [SerializeField] TextMeshProUGUI m_HP;
+	[SerializeField] UnityEngine.UI.Image m_Image;
 
     public override void Init(Player player)
     {
         m_Health = player.Health;
+		m_Health.HealthChanged += UpdateHP;
     }
 
-    void UpdateHP()
+    protected override void Update() { }
+    void UpdateHP(float value)
     {
         m_HP.text = $"{Mathf.RoundToInt(m_Health.Value)}";
-    }
-
-    protected override void Update()
-    {
-        UpdateHP();
+		m_Image.fillAmount = m_Health.Value / m_Health.Max;
     }
 }
